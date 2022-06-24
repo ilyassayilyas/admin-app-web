05ae7a07e91b0fdac26b32fc12158ff2668e62ba
Ilyas Bekpayev <ilyas.bekpayev@jits-3-24.jits.ipa.dc>
2022-06-24 08:23:32 +0600

    prefinal

--- src/components/UserTables/ScreeningUserTable/index.js
+++ src/components/UserTables/ScreeningUserTable/index.js
@@ -26,7 +26,7 @@ const ScreeningUserTable = (props) => {
         columns={columns}
         itemKey={(row) => row.id}
         rowHeight={35}
-        tableHeight={500}
+        tableHeight={400}
         sortColumn="fullName"
         sortDirection="ASC"
       />
